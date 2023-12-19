70e85c5393c7c5abb94298f17269e39ec8384889
Mayur Hanwate <frostblizard111@gmail.com>
2023-12-19 09:52:01 +0530

    g

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+import Weather from "./Weather";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "1b0a153a1e8e3418236afce6fa7462ca";
 
 const App = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+
+  function getSearch(e) {
+    e.preventDefault();
+
+    Axios.get(`${URL}${searchInput}&appid=${API_KEY}`)
+      .then((res) => setWeatherData(res))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={getSearch}>
+        <input
+          className="search"
+          type="text"
+          placeholder="Enter a city"
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </form>
+      <Weather data={weatherData} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
